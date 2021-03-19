1d173bb8a3028f926fee0ec9f8c03237df84626f
PRITHWIRAJ <prithwiraj10@gmail.com>
2021-03-19 18:13:07 +0530

    Added solution part1-Ex1.2

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -10,7 +10,11 @@ const Header = (props) => {
 
 const Content = (props) => {
   return (
-    <p>{props.part} {props.exercises}</p>
+    <div>
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part2} exercises={props.exercises2} />
+    </div>
   )
 }
 
@@ -19,21 +23,25 @@ const Total = (props) => {
     <p>Number of exercises {props.total}</p>
   )
 }
+
+const Part = (props) => {
+  return (
+    <p>{props.part} {props.exercises}</p>
+  )
+}
 function App() {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
   const exercises1 = 10
   const part2 = 'Using props to pass data'
   const exercises2 = 7
-  const part3 = 'State of a component'
+  // const part3 = 'State of a component'
   const exercises3 = 14
 
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
+      <Content part1={part1} part2={part2} exercises1={exercises1} exercises2={exercises2} />
       <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   );
