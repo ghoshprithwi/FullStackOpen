6781ac0cd5d8ac7deab31bfe18e3515e1660db74
PRITHWIRAJ <prithwiraj10@gmail.com>
2021-03-19 18:22:22 +0530

    Fixed mistake in App.js

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -13,7 +13,7 @@ const Content = (props) => {
     <div>
       <Part part={props.part1} exercises={props.exercises1} />
       <Part part={props.part2} exercises={props.exercises2} />
-      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
     </div>
   )
 }
@@ -35,13 +35,13 @@ function App() {
   const exercises1 = 10
   const part2 = 'Using props to pass data'
   const exercises2 = 7
-  // const part3 = 'State of a component'
+  const part3 = 'State of a component'
   const exercises3 = 14
 
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} part2={part2} exercises1={exercises1} exercises2={exercises2} />
+      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
       <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   );
