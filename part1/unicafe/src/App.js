0eecf5daff72fedcb57aa26c4e05043bd3eaf3d2
PRITHWIRAJ <prithwiraj10@gmail.com>
2021-03-23 23:51:21 +0530

    Added solution: part1_Ex1.6-Ex1.11

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,11 +1,79 @@
-import React from 'react';
+import React, { useState } from 'react'
 
-function App() {
+//Parent Statistics Component
+const Statistics = ({ allFeedback }) => {
+
+  if (allFeedback.good === 0 && allFeedback.neutral === 0 && allFeedback.bad === 0) {
+    return (
+      <tr><td>No feedback given.</td></tr>
+    )
+  }
+  const totalFeedback = allFeedback.good + allFeedback.neutral + allFeedback.bad;
+  const totalScore = (allFeedback.good + (allFeedback.bad * -1));
+
   return (
-    <div>
-      <p>Unicafe</p>
-    </div>
+    <>
+      <Statistic text="good" value={allFeedback.good} />
+      <Statistic text="neutral" value={allFeedback.neutral} />
+      <Statistic text="bad" value={allFeedback.bad} />
+      <Statistic text="all" value={totalFeedback} />
+      <Statistic text="average score" value={totalScore / totalFeedback} />
+      <Statistic text="positive" value={(allFeedback.good / totalFeedback) * 100 + " %"} />
+    </>
+  )
+}
+
+//Helper Component for displaying each statistic
+const Statistic = ({ text, value }) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
   );
 }
 
-export default App;
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const allFeedback = { good, neutral, bad };
+
+  const handleClickGood = () => {
+    setGood(good + 1);
+  }
+
+  const handleClickNeutral = () => {
+    setNeutral(neutral + 1);
+  }
+
+  const handleClickBad = () => {
+    setBad(bad + 1);
+  }
+
+  return (
+    <div>
+      <h1>Give Feedback</h1>
+      <Button handleClick={handleClickGood} text="good" />
+      <Button handleClick={handleClickNeutral} text="neutral" />
+      <Button handleClick={handleClickBad} text="bad" />
+      <h2>Statistics</h2>
+      <table>
+        <tbody>
+          <Statistics allFeedback={allFeedback} />
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default App
